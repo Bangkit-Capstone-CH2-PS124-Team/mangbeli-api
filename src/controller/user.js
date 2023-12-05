48c5ab04b06008da745ace985c6dae6fa41d62e8
Faiz Kurniawan <mbfzak1@gmail.com>
2023-12-05 22:43:51 +0700

    add edit profile user

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,4 +1,6 @@
+/* eslint-disable camelcase */
 import dbUsers from "../models/users.js";
+import bcrypt from "bcrypt";
 
 export const getUser = async (req, res) => {
     try {
@@ -21,6 +23,86 @@ export const getUser = async (req, res) => {
             error: false,
             message: "User fetched successfully",
             dataUser: user,
+        });
+    } catch (err) {
+        // console.error("[ERROR]", err);
+        res.status(500).json({
+            error: true,
+            message: "Internal Server Error",
+            errorMessage: err.message,
+        });
+    }
+};
+
+export const patchUser = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const {name, oldPassword, newPassword, no_hp, favorite} = req.body;
+
+        const user = await dbUsers.findOne({
+            where: {
+                userId,
+            },
+        });
+
+        if (oldPassword) {
+            if (!newPassword) {
+                return res.status(400).json({
+                    error: true,
+                    message: "newPassword must also be provided",
+                });
+            }
+
+            if (newPassword.length < 8) {
+                return res.status(400).json({
+                    error: true,
+                    message: "New Password must be at least 8 characters",
+                });
+            }
+
+            const match = await bcrypt.compare(oldPassword, user.password);
+
+            if (!match) {
+                return res.status(400).json({
+                    error: true,
+                    message: "Password and oldPassword do not match",
+                });
+            }
+
+            const salt = await bcrypt.genSalt();
+            const hashPassword = await bcrypt.hash(newPassword, salt);
+
+            await dbUsers.update(
+                {
+                    name,
+                    password: hashPassword,
+                    no_hp,
+                    favorite,
+                },
+                {
+                    where: {
+                        userId,
+                    },
+                },
+            );
+        } else {
+            await dbUsers.update(
+                {
+                    name,
+                    no_hp,
+                    favorite,
+                },
+                {
+                    where: {
+                        userId,
+                    },
+                },
+            );
+        }
+
+        res.json({
+            error: false,
+            message: "User updated successfully",
         });
     } catch (err) {
         // console.error("[ERROR]", err);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 /* eslint-disable new-cap */
 import express from "express";
-import {getUser} from "../controller/user.js";
+import {getUser, patchUser} from "../controller/user.js";
 import {verifyToken} from "../middleware/verify.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getUser);
+router.patch("/", verifyToken, patchUser);
 router.all("/", (req, res) => {
     res.status(405).json({
         error: true,

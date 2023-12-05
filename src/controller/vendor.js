12008b0856f96c5a3b9f8d6f1382609a417e484c
Faiz Kurniawan <mbfzak1@gmail.com>
2023-12-05 23:05:38 +0700

    add edit profile vendor

--- src/controller/vendor.js
+++ src/controller/vendor.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import dbVendors from "../models/vendors.js";
 
 export const getVendor = async (req, res) => {
@@ -21,6 +22,46 @@ export const getVendor = async (req, res) => {
             error: false,
             message: "Vendor fetched successfully",
             dataVendor: vendor,
+        });
+    } catch (err) {
+        // console.error("[ERROR]", err);
+        res.status(500).json({
+            error: true,
+            message: "Internal Server Error",
+            errorMessage: err.message,
+        });
+    }
+};
+
+export const patchVendor = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const role = req.role;
+        const {name_vendor, products, minPrice, maxPrice} = req.body;
+
+        if (role !== "vendor") {
+            return res.status(403).json({
+                error: true,
+                message: "Invalid role",
+            });
+        }
+
+        await dbVendors.update(
+            {
+                name_vendor,
+                products,
+                minPrice,
+                maxPrice,
+            },
+            {
+                where: {
+                    userId,
+                },
+            },
+        );
+        res.json({
+            error: false,
+            message: "Vendor updated successfully",
         });
     } catch (err) {
         // console.error("[ERROR]", err);
--- src/routes/vendor.js
+++ src/routes/vendor.js
@@ -1,11 +1,12 @@
 /* eslint-disable new-cap */
 import express from "express";
-import {getVendor} from "../controller/vendor.js";
+import {getVendor, patchVendor} from "../controller/vendor.js";
 import {verifyToken} from "../middleware/verify.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getVendor);
+router.patch("/", verifyToken, patchVendor);
 router.all("/", (req, res) => {
     res.status(405).json({
         error: true,
